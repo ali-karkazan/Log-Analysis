fe88481aa2b3af752a3dc844eaf3c6f99f84a1bb
Ali Karkazan <ali.karkazan@icloud.com>
2019-08-11 14:28:48 +0400

    Add question 2 solution

--- log.py
+++ log.py
@@ -14,12 +14,28 @@ def log_analysis():
         print(e.diag.message_detail)
         sys.exit(1)
     c = db.cursor()
+
+    # Question 1- top 3 articles (plz refer to the views in README.md file)
     views = """select count(*) as num_viewed, title from article_log
             group by title order by num_viewed desc limit 3;"""
     c.execute(views)
     views = c.fetchall()
+
+    print("\nThe most popular articles of all time:\n")
     for n in views:
-        print str(n[1]) + " " + str(n[0]) + " views"
+        print(' ' + str(n[1]) + " " + str(n[0]) + " views\n")
+
+    # Question 2- the author of all time
+    author_of_all_time = """select count(*) as author_of_all_time,
+                        name from author_name 
+                        group by name order by author_of_all_time desc;"""
+    c.execute(author_of_all_time)
+    author_of_all_time = c.fetchall()
+
+    print('\n The most popular authors of all time:\n')
+    for i in author_of_all_time:
+        print(' ' + str(i[1]) + " " + str(i[0]) + " views\n")
+
     db.close()
 
 log_analysis()
