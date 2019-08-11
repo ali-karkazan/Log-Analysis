28ab62c2e0263498b693718a0781486bc90079fe
Ali Karkazan <ali.karkazan@icloud.com>
2019-08-11 04:54:50 +0400

    final change to question 1 Adding article titles

--- log.py
+++ log.py
@@ -14,8 +14,8 @@ def log_analysis():
         print(e.diag.message_detail)
         sys.exit(1)
     c = db.cursor()
-    views = """select count(*) as num_viewed, slug from article_log
-            group by slug order by num_viewed desc limit 3;"""
+    views = """select count(*) as num_viewed, title from article_log
+            group by title order by num_viewed desc limit 3;"""
     c.execute(views)
     views = c.fetchall()
     for n in views:
